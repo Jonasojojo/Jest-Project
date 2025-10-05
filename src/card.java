61e88378f8d121c3386816ca4df1d4dc7a5aa66e
jonas <jonas180706@gmail.com>
2025-10-05 02:54:07 +0200

    🎉 created project and started card class

---
+++ src/card.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class card {
+    char suit;
+    int rank;
+
+    public card(char suit, int rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+    public char getSuit() {
+        return suit;
+    }
+    public int getRank() {
+        return rank;
+    }
+
+    public getValue () {
+        if ()
+    }
+}
